794bcc1c7338dce0fea81b1cdd063ed1899493b1
hats <109880147+hatsaregood@users.noreply.github.com>
2022-07-27 13:15:46 -0500

    you're an absolute idiot
    ITS "your" NOT "you're"

--- FPS.hx
+++ FPS.hx
@@ -47,7 +47,7 @@ class FPS extends TextField
 		currentFPS = 144;
 		selectable = false;
 		mouseEnabled = true;
-		defaultTextFormat =  new TextFormat (Paths.font("vcr.ttf"), 13, color); //this is how to change you're fonts, piss babies
+		defaultTextFormat =  new TextFormat (Paths.font("vcr.ttf"), 13, color); //this is how to change your fonts, piss babies //you do realize "you're" is suppose to be "your", correct?
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
